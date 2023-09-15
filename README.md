ba77b4ce39fcb34d9e364c03f737cd143fea8ef0
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-09-15 16:07:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-String
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-chbf6e)
---
+++ script.js
@@ -0,0 +1,19 @@
+// Challenge 1: Store the string "I am a" in one variable and store the other string "Kalvian" in other variable and concatenate both the string to get the output as "I am a Kalvain" using "+" operator.
+
+// Challenge 2: Store the given string in a variable and print the same in console. String: If you fail, never give up because FAIL means "First Attempt In Learning".
+
+// Challenge 3: Store a string in a variable and display the length of the string.
+
+// Challenge 4: Store a string in a variable and convert it into uppercase.
+
+//Challenge 5: Store the string "I love programming" in a variable and replace the word programming with "JavaScript programming".
+
+// Challenge 6: Store a string in variable and print the string in console 3 times using repeat method.
+
+//Challenge 7: Store the string as "I am a Kalvian" in a variable and convert the string into array and print the array in console.
+
+// Challenge 8: Store the given string "Time and Tide wait for none" in a variable and using indexOf method find the index of "Ti" and print the same in console.
+
+// Challenge 9: Store the given string "When life gives you lemons make lemonade" in a variable and find whether it includes "lemon".
+
+// Challenge 10: Store the given string "You must be the change you wish to see in the world." in a variable and use slice to console log the word "change" and use substring method to console log the word "world".
